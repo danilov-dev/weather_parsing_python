1ca3192663d4d598ec5f72581f21d11c468a6a01
danilov-dev <dan.forth.ant@gmail.com>
2026-02-25 17:59:12 +0300

    Added the ability to write to a file

--- main.py
+++ main.py
@@ -4,4 +4,14 @@ if __name__ == "__main__":
     base_url = "https://world-weather.ru/pogoda/russia/saint_petersburg/"
     parser = Parser(base_url)
     json_weather_data = parser.parse()
+    if json_weather_data is not None:
+        try:
+            with open('paring_result.json', 'w', encoding='utf-8') as outfile:
+                outfile.write(json_weather_data)
+        except  Exception as e:
+            print(f"Error occurred {e}")
+    else:
+        print("Не удалось получить данные о погоде")
+
+
     print(json_weather_data)
